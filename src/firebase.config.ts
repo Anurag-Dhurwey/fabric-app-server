582bc51c06deb617f469c4296d6f404ca1875741
Anurag-Dhurwey <anuragdhurwey9211@gmail.com>
2024-03-01 18:54:46 +0530

    firebase envirment is added

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,4 +1,3 @@
-
 import admin from "firebase-admin";
 require("dotenv").config();
 const {
@@ -16,23 +15,29 @@ const {
   databaseUrl,
 } = process.env;
 
-const serviceAccount = {
-  type: `"${type}"`,
-  project_id: `"${project_id}"`,
-  private_key_id: `"${private_key_id}"`,
-  private_key: `"${private_key}"`,
-  client_email: `"${client_email}"`,
-  client_id: `"${client_id}"`,
-  auth_uri: `"${auth_uri}"`,
-  token_uri: `"${token_uri}"`,
-  auth_provider_x509_cert_url: `"${auth_provider_x509_cert_url}"`,
-  client_x509_cert_url: `"${client_x509_cert_url}"`,
-  universe_domain: `"${universe_domain}"`,
-};
+const {privateKey}=JSON.parse(private_key!)
+
+// const serviceAccount = {
+//   type,
+//   project_id,
+//   private_key_id,
+//   // private_key,
+//   client_email,
+//   client_id,
+//   auth_uri,
+//   token_uri,
+//   auth_provider_x509_cert_url,
+//   client_x509_cert_url,
+//   universe_domain,
+// };
 
 export const initialize_firebase = () => {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as any),
+    credential: admin.credential.cert({
+      projectId: project_id,
+      clientEmail: client_email,
+      privateKey
+    }),
     databaseURL: databaseUrl,
   });
 };
