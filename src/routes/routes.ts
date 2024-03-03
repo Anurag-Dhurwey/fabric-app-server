4fa032cde02dbad7520379a5e4d7d56c5fc2e67e
Anurag-Dhurwey <anuragdhurwey9211@gmail.com>
2024-03-03 19:16:38 +0530

    nothing

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,20 +1,25 @@
 import { Router } from "express";
-import { admin } from "../firebase.config";
+import { db,admin } from "../firebase.config";
 
 const routes = Router();
 
-routes.post("/signup",async (req, res) => {
-//   admin.auth().createUser({
-//     email: "user@example.com",
-//     password: "password123",
-//   })
-//   .then((user) => {
-//    res.json(user)
-//   })
-//   .catch((error) => {
-//     console.error("Error creating new user:", error);
-//     res.status(404).json(error)
-//   });
+routes.post("/test",async (req, res) => {
+  const data = {
+    name: "John Doe",
+    age: 30,
+    city: "New York"
+  };
+  
+  db.collection('users').add(data)
+    .then(ref => {
+      console.log('Document written with ID:', ref.id);
+
+    })
+    .catch(err => {
+      // res.json({message:'error'})
+      console.error('Error adding document:', err);
+    });
+  res.json({message:'done'})
   
 });
 routes.post("/login",async (req, res) => {
